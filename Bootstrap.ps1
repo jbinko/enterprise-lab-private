9bab4617dc0d785ac9d5416226cea1c8332a7151
jbinko <jbinko+github@gmail.com>
2025-11-28 19:17:58 +0100

    c

---
+++ Bootstrap.ps1
@@ -0,0 +1,11 @@
+Start-Transcript -Path c:\Bootstrap.log
+
+# Formatting VMs disk
+$disk = (Get-Disk | Where-Object partitionstyle -eq 'raw')[0]
+$driveLetter = "F"
+$label = "VMsDisk"
+$disk | Initialize-Disk -PartitionStyle MBR -PassThru | `
+    New-Partition -UseMaximumSize -DriveLetter $driveLetter | `
+    Format-Volume -FileSystem NTFS -NewFileSystemLabel $label -Confirm:$false -Force
+
+Stop-Transcript
