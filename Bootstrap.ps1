b0d80ca48e68aed9fd39727a344ff62d03d53899
jbinko <jbinko+github@gmail.com>
2025-11-29 12:14:21 +0100

    web srv

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -120,13 +120,20 @@ Write-Host "Creating and installing lab"
 # Get-LabAvailableOperatingSystem
 $labName = 'MyEnterpriseLab'
 $labDomainName = 'MyEnterpriseLab.net'
+$labDnsServer1 = '192.168.84.10'
 $labSources = 'F:\LabSources'
 New-LabDefinition -Name $labName -DefaultVirtualizationEngine HyperV -VmPath F:\VMs
 Add-LabVirtualNetworkDefinition -Name $labName -AddressSpace 192.168.84.0/24
 Set-LabInstallationCredential -Username $windowsAdminUsername -Password $windowsAdminPassword
+# DC
 Add-LabDomainDefinition -Name $labDomainName -AdminUser $windowsAdminUsername -AdminPassword $windowsAdminPassword
 Add-LabMachineDefinition -Name DC1 -Memory 3GB -Network $labName -IpAddress 192.168.84.10 `
-    -DnsServer1 192.168.84.10 -DomainName $labDomainName -Roles RootDC `
+    -DnsServer1 $labDnsServer1 -DomainName $labDomainName -Roles RootDC `
+    -ToolsPath $labSources\Tools -OperatingSystem 'Windows Server 2025 Standard (Desktop Experience)'
+# WEB
+$role = Get-LabMachineRoleDefinition -Role WebServer -Properties @{ OrganizationName = 'Marketing' }
+Add-LabMachineDefinition -Name WEB01 -Memory 3GB -Network $labName -IpAddress 192.168.84.20 `
+    -DnsServer1 $labDnsServer1 -DomainName $labDomainName -Roles $role `
     -ToolsPath $labSources\Tools -OperatingSystem 'Windows Server 2025 Standard (Desktop Experience)'
 Install-Lab
 Show-LabDeploymentSummary
