31d9ab7c06b562f10cac11e476afa76fdf84a734
jbinko <jbinko+github@gmail.com>
2025-11-29 17:31:13 +0100

    c

--- scripts/Bootstrap.ps1
+++ scripts/Bootstrap.ps1
@@ -21,94 +21,21 @@ $disk | Initialize-Disk -PartitionStyle MBR -PassThru | `
 Write-Host "Extending C:\ partition to the maximum size"
 Resize-Partition -DriveLetter C -Size $(Get-PartitionSupportedSize -DriveLetter C).SizeMax
 
-# Downloading scripts
-Write-Host "Downloading scripts"
-$scriptsDir = "F:\Scripts"
-New-Item -ItemType Directory -Path $scriptsDir -Force | Out-Null
-Invoke-WebRequest ($artifactsBaseUrl + "scripts/RunAfterRestart.ps1") -OutFile $scriptsDir\RunAfterRestart.ps1
-
-# Disable Microsoft Edge sidebar
-Write-Host "Disabling Microsoft Edge sidebar"
-$RegistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Edge'
-$Name = 'HubsSidebarEnabled'
-$Value = '00000000'
-# Create the key if it does not exist
-If (-NOT (Test-Path $RegistryPath)) {
-    New-Item -Path $RegistryPath -Force | Out-Null
-}
-New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD -Force
-
-# Disable Microsoft Edge first-run Welcome screen
-Write-Host "Disabling Microsoft Edge first-run Welcome screen"
-$RegistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Edge'
-$Name = 'HideFirstRunExperience'
-$Value = '00000001'
-# Create the key if it does not exist
-If (-NOT (Test-Path $RegistryPath)) {
-    New-Item -Path $RegistryPath -Force | Out-Null
-}
-New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD -Force
-
-# Set Diagnostic Data settings
-Write-Host "Setting Diagnostic Data settings via registry"
-$telemetryPath = "HKLM:\Software\Policies\Microsoft\Windows\DataCollection"
-$telemetryProperty = "AllowTelemetry"
-$telemetryValue = 0 # 0 - Disable telemetry
-# Create the registry key and set the value for AllowTelemetry
-if (-not (Test-Path $telemetryPath)) {
-    New-Item -Path $telemetryPath -Force | Out-Null
-}
-Set-ItemProperty -Path $telemetryPath -Name $telemetryProperty -Value $telemetryValue
-
-$oobePath = "HKLM:\Software\Policies\Microsoft\Windows\OOBE"
-$oobeProperty = "DisablePrivacyExperience"
-$oobeValue = 1
-# Create the registry key and set the value for DisablePrivacyExperience
-if (-not (Test-Path $oobePath)) {
-    New-Item -Path $oobePath -Force | Out-Null
-}
-Set-ItemProperty -Path $oobePath -Name $oobeProperty -Value $oobeValue
-
-Write-Host "Registry keys and values for Diagnostic Data settings have been set successfully."
-
-# Register schedule task to run after system reboot
-#Write-Host "Registering scheduled task to run after system reboot"
-#$Trigger = New-ScheduledTaskTrigger -AtStartup
-#$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "$scriptsDir\RunAfterRestart.ps1"
-#Register-ScheduledTask -TaskName "RunAfterRestart" -Trigger $Trigger -User SYSTEM -Action $Action -RunLevel "Highest" -Force
-#Write-Host "Registered scheduled task 'RunAfterRestart' to run after system reboot."
-
-# Install AutomatedLab
-Write-Host "Installing AutomatedLab"
-Install-PackageProvider Nuget -Force -Confirm:$False
-Install-Module PSFramework -SkipPublisherCheck -Force -Confirm:$False -AllowClobber
-Install-Module AutomatedLab -SkipPublisherCheck -Force -Confirm:$False -AllowClobber
-
-#  Disable (which is already the default) and in addition skip dialog
-[Environment]::SetEnvironmentVariable('AUTOMATEDLAB_TELEMETRY_OPTIN', 'false', 'Machine')
-$env:AUTOMATEDLAB_TELEMETRY_OPTIN = 'false'
-
-# Pre-configure Lab Host Remoting
-Enable-LabHostRemoting -Force
-
-New-LabSourcesFolder -DriveLetter F
 
 
 
 
+# Disabling Windows Server Manager Scheduled Task
+Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
 
-
-
-
-
-
-# Install Hyper-V
-Write-Host "Installing Hyper-V"
+# Install Hyper-V and reboot
+Write-Host "Installing Hyper-V and restart"
 Enable-WindowsOptionalFeature -Online -FeatureName Containers -All -NoRestart
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
 Install-WindowsFeature -Name Hyper-V -IncludeAllSubFeature -IncludeManagementTools -Restart
 
-Stop-Transcript
-
 # Restart computer
 Restart-Computer
+
+Stop-Transcript
+
