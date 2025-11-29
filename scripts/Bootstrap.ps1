e95cba59492c8900b19165205718967905d304ff
jbinko <jbinko+github@gmail.com>
2025-11-29 16:28:57 +0100

    c

--- scripts/Bootstrap.ps1
+++ scripts/Bootstrap.ps1
@@ -93,37 +93,14 @@ Enable-LabHostRemoting -Force
 
 New-LabSourcesFolder -DriveLetter F
 
-Write-Host "Starting parallel jobs"
-$jobs = @()
 
-Write-Host "Installing PowerShell 7"
-$jobs += Start-Job -ScriptBlock {
-    $ProgressPreference = 'SilentlyContinue'
-    $url = "https://github.com/PowerShell/PowerShell/releases/latest"
-    $latestVersion = (Invoke-WebRequest -UseBasicParsing -Uri $url).Content | Select-String -Pattern "v[0-9]+\.[0-9]+\.[0-9]+" | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value
-    $downloadUrl = "https://github.com/PowerShell/PowerShell/releases/download/$latestVersion/PowerShell-$($latestVersion.Substring(1,5))-win-x64.msi"
-    Invoke-WebRequest -UseBasicParsing -Uri $downloadUrl -OutFile .\PowerShell7.msi
-    Start-Process msiexec.exe -Wait -ArgumentList '/I PowerShell7.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1'
-    Remove-Item .\PowerShell7.msi
-}
 
-# Download ISOs
-Write-Host "Downloading ISOs in parallel..."
-# Decode base64 and convert JSON string to PowerShell object
-$isoList = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($isoDownloadsBase64Json)) | ConvertFrom-Json
-$targetDir = "F:\LabSources\ISOs"
 
-foreach ($iso in $isoList) {
-    $jobs += Start-Job -ScriptBlock {
-        param($url, $dir, $name)
-        $filePath = Join-Path $dir $name
-        Write-Host "Downloading $url to $filePath"
-        Invoke-WebRequest -Uri $url -OutFile $filePath
-    } -ArgumentList $iso.isoDownloadUrl, $targetDir, $iso.name
-}
-# Wait for completion
-$jobs | ForEach-Object { $_ | Wait-Job; Receive-Job $_; Remove-Job $_ }
-Write-Host "All parallel jobs completed."
+
+
+
+
+
 
 # Install Hyper-V
 Write-Host "Installing Hyper-V"
