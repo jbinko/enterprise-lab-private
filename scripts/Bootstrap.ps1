37eea53ff28f127d5765932cc13d205ab2e4ba39
jbinko <jbinko+github@gmail.com>
2025-11-29 23:24:26 +0100

    c

--- scripts/Bootstrap.ps1
+++ scripts/Bootstrap.ps1
@@ -109,12 +109,19 @@ Write-Host "Downloading ISOs..."
 $isoList = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($isoDownloadsBase64Json)) | ConvertFrom-Json
 $targetDir = "F:\LabSources\ISOs"
 
+# TODO NECHCEME PARALENI STAHOVANI?
+$jobs = @()
 foreach ($iso in $isoList) {
-    $filePath = Join-Path $targetDir $iso.name
-    Write-Host "Downloading $iso.isoDownloadUrl to $filePath"
-    Invoke-WebRequest -Uri $iso.isoDownloadUrl -OutFile $filePath
+    $jobs += Start-Job -ScriptBlock {
+        param($url, $dir, $name)
+        $filePath = Join-Path $dir $name
+        Write-Host "Downloading $url to $filePath"
+        Invoke-WebRequest -Uri $url -OutFile $filePath
+    } -ArgumentList $iso.isoDownloadUrl, $targetDir, $iso.name
 }
 
+# Wait for completion
+$jobs | ForEach-Object { $_ | Wait-Job; Receive-Job $_; Remove-Job $_ }
 
 
 
