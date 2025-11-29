5a538f4eb55ed4385b89d0afbc53b02983206cba
jbinko <jbinko+github@gmail.com>
2025-11-29 21:43:06 +0100

    c

--- scripts/Bootstrap.ps1
+++ scripts/Bootstrap.ps1
@@ -91,7 +91,7 @@ New-LabSourcesFolder -DriveLetter F
 
 
 
-
+<#
 Write-Host "Installing PowerShell 7"
 $ProgressPreference = 'SilentlyContinue'
 $url = "https://github.com/PowerShell/PowerShell/releases/latest"
@@ -100,7 +100,7 @@ $downloadUrl = "https://github.com/PowerShell/PowerShell/releases/download/$late
 Invoke-WebRequest -UseBasicParsing -Uri $downloadUrl -OutFile .\PowerShell7.msi
 Start-Process msiexec.exe -Wait -ArgumentList '/I PowerShell7.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1'
 Remove-Item .\PowerShell7.msi
-
+#>
 
 
 # Download ISOs
