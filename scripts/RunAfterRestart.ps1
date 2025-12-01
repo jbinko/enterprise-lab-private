d7f93c5812ec8657268ef65d4c808aa9d20bd070
jbinko <jbinko+github@gmail.com>
2025-12-01 21:57:46 +0100

    NAT

--- scripts/RunAfterRestart.ps1
+++ scripts/RunAfterRestart.ps1
@@ -6,6 +6,13 @@ param (
 $TranscriptFile = "c:\Bootstrap.log"
 Start-Transcript -Path $TranscriptFile -Append
 
+
+# Create LabNATSwitch
+$LabNATSwitch = "LabNATSwitch"
+New-VMSwitch -SwitchName $LabNATSwitch -SwitchType Internal
+New-NetIPAddress -IPAddress 192.168.1.1 -PrefixLength 24 -InterfaceAlias "vEthernet ($LabNATSwitch)"
+New-NetNat -Name LabNATNetwork -InternalIPInterfaceAddressPrefix 192.168.1.0/24
+
 # Headless/Non-interactive Environments: Disable all prompts
 # Turn off telemetry, do not sync lab sources content
 # Set-PSFConfig -FullName AutomatedLab.DoNotPrompt -Value $true -PassThru | Register-PSFConfig
@@ -16,10 +23,12 @@ Write-Host "Creating and installing lab"
 # Get-LabAvailableOperatingSystem -Path F:\LabSources
 $labName = 'MyEnterpriseLab'
 $labDomainName = 'MyEnterpriseLab.net'
-$labDnsServer1 = '192.168.84.10'
+$labDnsServer1 = '192.168.10.10'
+$labTimeZone = 'Central Europe Standard Time'
 $labSources = 'F:\LabSources'
 New-LabDefinition -Name $labName -DefaultVirtualizationEngine HyperV -VmPath F:\VMs
-Add-LabVirtualNetworkDefinition -Name $labName -AddressSpace 192.168.84.0/24
+Add-LabVirtualNetworkDefinition -Name $labName -AddressSpace 192.168.10.0/24
+Add-LabVirtualNetworkDefinition -Name $LabNATSwitch -HyperVProperties @{ SwitchType = 'Internal' }
 
 Add-LabIsoImageDefinition -Name SQLServer2012 -Path $labSources\ISOs\en_sql_server_2012_standard_edition_x86_x64_dvd_813403.iso
 
@@ -27,22 +36,30 @@ Add-LabIsoImageDefinition -Name SQLServer2012 -Path $labSources\ISOs\en_sql_serv
 Add-LabDomainDefinition -Name $labDomainName -AdminUser $windowsAdminUsername -AdminPassword $windowsAdminPassword
 Set-LabInstallationCredential -Username $windowsAdminUsername -Password $windowsAdminPassword
 Add-LabMachineDefinition -Name DC01 -Memory 3GB -Network $labName -IpAddress $labDnsServer1 `
-    -DnsServer1 $labDnsServer1 -DomainName $labDomainName -Roles RootDC -TimeZone "Central Europe Standard Time" `
+    -DnsServer1 $labDnsServer1 -DomainName $labDomainName -Roles RootDC -TimeZone $labTimeZone `
     -ToolsPath $labSources\Tools -OperatingSystem 'Windows Server 2025 Standard (Desktop Experience)'
 
+# Router
+$netAdapter = @()
+$netAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch $labName
+$netAdapter += New-LabNetworkAdapterDefinition -VirtualSwitch $LabNATSwitch -UseDhcp
+Add-LabMachineDefinition -Name Router01 -Memory 3GB -NetworkAdapter $netAdapter `
+    -DnsServer1 $labDnsServer1 -DomainName $labDomainName -Roles Routing -TimeZone $labTimeZone `
+    -ToolsPath $labSources\Tools -OperatingSystem 'Windows Server 2012 Standard (Server with a GUI)'
+
 # SQL
-Add-LabMachineDefinition -Name SQL01 -Memory 3GB -Network $labName -IpAddress 192.168.84.25 `
-    -DnsServer1 $labDnsServer1 -DomainName $labDomainName -Roles SQLServer2012 -TimeZone "Central Europe Standard Time" `
+Add-LabMachineDefinition -Name SQL01 -Memory 3GB -Network $labName -IpAddress 192.168.10.25 `
+    -DnsServer1 $labDnsServer1 -DomainName $labDomainName -Roles SQLServer2012 -TimeZone $labTimeZone `
     -ToolsPath $labSources\Tools -OperatingSystem 'Windows Server 2012 Standard (Server with a GUI)'
 
 # FS
-Add-LabMachineDefinition -Name FS01 -Memory 3GB -Network $labName -IpAddress 192.168.84.15 `
-    -DnsServer1 $labDnsServer1 -DomainName $labDomainName -Roles FileServer -TimeZone "Central Europe Standard Time" `
+Add-LabMachineDefinition -Name FS01 -Memory 3GB -Network $labName -IpAddress 192.168.10.15 `
+    -DnsServer1 $labDnsServer1 -DomainName $labDomainName -Roles FileServer -TimeZone $labTimeZone `
     -ToolsPath $labSources\Tools -OperatingSystem 'Windows Server 2012 Standard (Server with a GUI)'
 
 # WEB
-Add-LabMachineDefinition -Name WEB01 -Memory 3GB -Network $labName -IpAddress 192.168.84.20 `
-    -DnsServer1 $labDnsServer1 -DomainName $labDomainName -Roles WebServer -TimeZone "Central Europe Standard Time" `
+Add-LabMachineDefinition -Name WEB01 -Memory 3GB -Network $labName -IpAddress 192.168.10.20 `
+    -DnsServer1 $labDnsServer1 -DomainName $labDomainName -Roles WebServer -TimeZone $labTimeZone `
     -ToolsPath $labSources\Tools -OperatingSystem 'Windows Server 2012 Standard (Server with a GUI)'
 
 <#
@@ -52,11 +69,11 @@ Add-LabMachineDefinition -Name WEB01 -Memory 3GB -Network $labName -IpAddress 19
 
 
 $role = Get-LabMachineRoleDefinition -Role WebServer -Properties @{ OrganizationName = 'Marketing' }
-Add-LabMachineDefinition -Name WEB01 -Memory 3GB -Network $labName -IpAddress 192.168.84.20 `
+Add-LabMachineDefinition -Name WEB01 -Memory 3GB -Network $labName -IpAddress 192.168.10.20 `
     -DnsServer1 $labDnsServer1 -DomainName $labDomainName -Roles $role `
     -ToolsPath $labSources\Tools -OperatingSystem 'Windows Server 2012 Standard (Server with a GUI)'
 
-Add-LabMachineDefinition -Name UBU01 -Memory 3GB -Network $labName -IpAddress 192.168.14.10 `
+Add-LabMachineDefinition -Name UBU01 -Memory 3GB -Network $labName -IpAddress 192.168.10.10 `
     -DnsServer1 $labDnsServer1 `
     -OperatingSystem 'Ubuntu-Server 24.04.3 LTS "Noble Numbat"' -UbuntuPackage Minimal
 #>
